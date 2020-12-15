ac1153fc09445b9beeef7a878f9d1019e07c6ac9
Liutauras Vilda <liutauras.vilda@gmail.com>
2020-12-15 02:08:53 +0000

    day 14 part2 almost done

--- racket/day14.rkt
+++ racket/day14.rkt
@@ -48,4 +48,44 @@
            (bitmask (cdr input) (hash-set memory loc (mask-map mask val)) mask))]))
 
 ; part1
-(bitmask input (make-immutable-hash) "")
+;(bitmask input (make-immutable-hash) "")
+
+; part2
+(define (mask-mapper-v2 m b acc)
+  (cond [(null? m) acc]
+        [(equal? (car m) "0") (mask-mapper-v2 (cdr m) (cdr b) (append acc (list (car b))))]
+        [(equal? (car m) "1") (mask-mapper-v2 (cdr m) (cdr b) (append acc (list 1)))]
+        [else (mask-mapper-v2 (cdr m) (cdr b) (append acc (list (car m))))]))
+
+(check-equal? (mask-mapper-v2 (map string (string->list "000000000000000000000000000000X1001X")) (dec->bin 42) '())
+              '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "X" 1 1 0 1 "X"))
+
+(define (get-combinations m)
+  ; TODO
+  (void))
+
+(check-equal? (get-combinations '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "X" 1 1 0 1 "X"))
+               '('(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0)
+                 '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 1)
+                 '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 0)
+                 '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 1 1)))
+
+(define (mask-map-v2 mask value)
+  (let* ([m (map string (string->list mask))]
+        [b (dec->bin value)]
+        [mapped (mask-mapper-v2 m b '())])
+    (map (λ (e) (bin->dec (b->n e)) (get-combinations mapped)))))
+
+(define (update-locs memory locs val)
+  (cond [(null? locs) memory]
+        [else (update-locs (hash-set memory (car locs) val)  (cdr locs) val)]))
+
+(define (bitmask-v2 input memory mask)
+  (cond [(null? input) (apply + (hash-values memory))]
+        [(string-contains? (car input) "mask") (bitmask-v2 (cdr input) memory (substring (car input) 7))]
+        [else
+         (let* ([loc (substring (car input) 4 (string-contains (car input) "]"))]
+               [val (string->number (substring (car input) (+ (string-contains (car input) "=") 2)))]
+               [locs-to-update (mask-map-v2 mask loc)]
+               [new-memory (update-locs memory locs-to-update val)])
+           (bitmask-v2 (cdr input) new-memory mask))]))
